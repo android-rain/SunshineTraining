aec1efceb9b1ae86ffc1af37079c74bc75d0477f
gitGeek <yy924224@sina.com>
2015-06-13 20:37:18 +0800

    add share function by myself

--- java/com/yun/android/sunshine/app/DetailActivity.java
+++ java/com/yun/android/sunshine/app/DetailActivity.java
@@ -2,7 +2,9 @@ package com.yun.android.sunshine.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,17 +12,40 @@ import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
 
+    private ShareActionProvider mShareActionProvider;
     private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        textView = (TextView)findViewById(R.id.detailText);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+
+        //Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        //Fetch and store shareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+
+        sendIntent.putExtra(Intent.EXTRA_TEXT, textView.getText());
+        sendIntent.setType("text/plain");
+        setShareIntent(sendIntent);
+//        startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.send_to)));
         return true;
+    }
+
+    //call to update the share intent
+    private void setShareIntent(Intent shareIntent){
+        if (mShareActionProvider != null){
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+
     }
 
     @Override
@@ -38,5 +63,6 @@ public class DetailActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 
 }
